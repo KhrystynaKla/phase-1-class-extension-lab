c1cc03811155a260f573cc6302294d7394b43120
Khrystyna Klapushchak <klapushchak.khr@gmail.com>
2023-10-19 13:44:51 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Polygon{
+    constructor(array){
+        this.sides = array
+    }
+    get countSides(){
+        return this.sides.length
+    }
+
+    get perimeter (){
+        return this.sides.reduce((a,b)=>a+b,0)
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid(){
+        if (this.countSides===3){
+            if ((this.sides[0]+this.sides[1]>this.sides[2])&& (this.sides[1]+this.sides[2]>this.sides[0])&&(this.sides[0]+this.sides[2]>this.sides[1])) return true;  
+        } 
+        return false;
+    }
+}
+
+class Square extends Polygon {
+    get isValid(){
+        if((this.sides[0]===this.sides[1])&& (this.sides[1]===this.sides[2])&& (this.sides[2]===this.sides[3]) && (this.countSides===4)) return true;
+        return false;
+    }
+    get area(){
+        return this.sides[0]**2
+
+    }
+}
